c8dfb16bd4440d2e0ee065337b53fdf0eaa7949a
David Arrizon <arrizon.david@gmail.com>
2020-01-07 04:23:58 -0600

    added login form

--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 import  Menu  from './components/Menu.js'
+import Login from './components/Login.js'
 
 function App() {
   return (
-    <Router>
-    
-      <Menu />
-
-
-      
-   
-    </Router>
+		<Router>
+			<>
+				<Menu />
+				<Login />
+			</>
+		</Router>
   );
 }
 
