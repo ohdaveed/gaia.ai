93adde753566c8c55dd4e9a9b1e4cd27ab5cd1b2
David Arrizon <arrizon.david@gmail.com>
2020-01-22 16:01:44 -0600

    fixe dlanding page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 import  Menu  from './components/Menu.js'
 import Login from './components/Login.js'
+import Landing from './components/Landing.js'
+import Register from './components/Register.js'
 
 function App() {
   return (
 		<Router>
 			<>
 				<Menu />
+        <Landing />
 				<Login />
+        <Register />
 			</>
 		</Router>
   );
