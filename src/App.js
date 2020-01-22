9c1a3241211ef8800f674e3e8375db9bbc072b66
David Arrizon <arrizon.david@gmail.com>
2020-01-22 20:47:22 +0000

    added react router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 import  Menu  from './components/Menu.js'
 import Login from './components/Login.js'
 import Landing from './components/Landing.js'
 import Register from './components/Register.js'
+// import Router from './components/Router.js'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
+
 
 function App() {
   return (
-		<Router>
-			<>
-				<Menu />
-				<Login />
-                <Landing />
-                <Register />
-			</>
-		</Router>
+      <Router>
+		   <Switch>
+          <Route path="/landing">
+            <Landing />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/">
+            <Landing />
+          </Route>
+        </Switch>
+    </Router>
   );
 }
 
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Card, CardTitle, CardText, CardImg, CardImgOverlay, Container, Button } from 'react-bootstrap';
+import Menu from './Menu.js'
 
 const Landing = (props) => {
   return (
       <Container>
+          <Menu />
       <Card className="bg-dark text-white">
   <Card.Img src="https://res.cloudinary.com/darrizon/image/upload/v1579651896/background-omg/green-leaves.jpg" alt="leaves bg"/>
   <Card.ImgOverlay>
