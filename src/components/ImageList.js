edb013356e66f183fa62899255a7e1483311ce4b
David Arrizon <arrizon.david@gmail.com>
2020-03-30 15:08:37 -0500

    added imagelist comp

---
+++ src/components/ImageList.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ImageList = (props) => {
+
+    const token = JSON.parse(localStorage.getItem('tokens'))
+
+    const list = (token.payload.url)
+    
+
+    return (
+       
+      list
+      
+    )
+  }
+
+export default ImageList
