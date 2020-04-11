1c91954c6296cc40250afb27ddefaae57340d4b4
David Arrizon <arrizon.david@gmail.com>
2020-04-10 19:27:51 -0500

    can display all uploaded photos

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,23 +1,62 @@
 /* eslint-disable no-unused-vars */
 import React, { useState, useContext, useEffect } from 'react';
-import ImageList from './ImageList';
+// import ImageList from './ImageList';
 import { authContext } from '../context/AuthContext';
-import {
-  Image,
-  Video,
-  Transformation,
-  CloudinaryContext,
-} from 'cloudinary-react';
+// import {
+//   Image,
+//   Video,
+//   Transformation,
+//   CloudinaryContext,
+// } from 'cloudinary-react';
+// import { getDefaultNormalizer } from '@testing-library/react';
+import { Image } from 'react-bootstrap';
 
 const Gallery = (props) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const { setAuthData, auth } = useContext(authContext);
+
+  useEffect(() => {
+    setLoading(true);
+
+    async function fetchData() {
+      try {
+        const requestOptions = {
+          method: 'GET',
+          headers: { Authorization: 'Bearer ' + auth.data },
+        };
+
+        const response = await fetch(
+          'http://penguin.linux.test:8000/api/photos/all',
+          requestOptions,
+        );
+
+        const data = await response.json();
+        setData(data);
+      } catch (err) {
+        console.log('err');
+        throw new Error(err);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
     <>
-      <div
-        style={{ height: '100vh' }}
-        className="d-flex justify-content-center align-items-center"
-      >
-        <div style={{ width: 500 }}></div>
-      </div>
+      {loading ? (
+        <div>
+          <h3>Loading Data...</h3>
+        </div>
+      ) : (
+        data.map((photo) => (
+          <div key={photo.id}>
+            <h3>{photo.url}</h3>
+            <img src={photo.url} width="250" height="225" />
+          </div>
+        ))
+      )}
     </>
   );
 };
