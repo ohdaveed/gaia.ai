8fafdefaf2b913c178a25cb8040b1bb00b94af8e
David Arrizon <arrizon.david@gmail.com>
2021-06-23 00:09:53 -0700

    removed unncessary html elements

---
+++ src/components/Menu.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Navbar , NavDropdown,  Nav , Form , Button, FormControl, Container } from 'react-bootstrap'
+
+function Menu() {
+  return (
+    <>
+    <Container>
+	<Navbar bg="light" expand="lg">
+		<Navbar.Brand href="#home">GAIA.ai</Navbar.Brand>
+		<Navbar.Toggle aria-controls="basic-navbar-nav" />
+		<Navbar.Collapse id="basic-navbar-nav">
+			<Nav className="mr-auto">
+				<Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#login">Login</Nav.Link>
+				<Nav.Link href="#register">Register</Nav.Link>
+			</Nav>
+		</Navbar.Collapse>
+	</Navbar>
+</Container>
+</>
+  )}
+
+export default Menu
