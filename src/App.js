21bb302a782cac0d25175a0dd602f468dc6a97b8
David Arrizon <arrizon.david@gmail.com>
2022-06-06 23:41:55 -0700

    june

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { Container } from "react-bootstrap";
-
 import Header from "./components/Header";
 import Body from "./components/Body";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 function App() {
   return (
